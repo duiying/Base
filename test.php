ef8dd44e48cdb1966a1b7e9f854299bec8906d67
wangyaxian <wangyaxiandev@gmail.com>
2021-10-09 17:00:42 +0800

    消息队列

--- test.php
+++ test.php
@@ -1,11 +1,37 @@
 <?php
 
-// ftok：Convert a pathname and a project identifier to a System V IPC key（把一个路径和标识符转换成 IPC 的 key）
 $key = ftok(__FILE__, 'a');
-// msg_get_queue：Create or attach to a message queue（根据传入的键创建或返回一个消息队列的引用）
 $queue = msg_get_queue($key);
 
-// 往消息队列发送一条消息
-msg_send($queue, 2, 'hello');
+$pid = pcntl_fork();
 
-echo '消息队列发送成功' . PHP_EOL;
+if ($pid === 0) {
+    while (1) {
+        // MSG_IPC_NOWAIT：非阻塞，立即返回；（如果使用非阻塞方式，该函数调用的次数会非常高，所以占用 CPU 资源就会高）
+        msg_receive($queue, 0, $msgType, 1024, $msg, true, MSG_IPC_NOWAIT, $error);
+        if ($error != MSG_ENOMSG) {
+            echo "msgType：$msgType msg：$msg" . PHP_EOL;
+        }
+    }
+}
+
+$i = 0;
+
+while (1) {
+    if ($i++ === 3) {
+        posix_kill($pid, SIGKILL);
+        break;
+    }
+    // 往消息队列发送一条消息
+    msg_send($queue, 2, 'hello', true);
+    sleep(1);
+}
+
+// 回收子进程
+$pid = pcntl_wait($status);
+if ($pid > 0) {
+    echo "子进程 $pid 退出了" . PHP_EOL;
+}
+
+// 删除消息队列
+msg_remove_queue($queue);
--- test2.php
+++ test2.php
@@ -1,32 +1,7 @@
 <?php
 
-echo sprintf('进程启动了，pid：%d' . PHP_EOL, posix_getpid());
+$key = ftok('/home/work/www/test.php', 'a');
+$queue = msg_get_queue($key);
 
-function printId()
-{
-    $pid = posix_getpid();
-    // sid：会话 ID；pgid：进程组长 ID；
-    echo sprintf('pid=%d ppid=%d pgid=%d sid=%d' . PHP_EOL, posix_getpid(), posix_getppid(), posix_getpgrp(), posix_getsid($pid));
-}
-
-$pid = pcntl_fork();
-
-printId();
-
-// 父进程退出
-if ($pid > 0) exit();
-
-// 创建会话，并将自己设置为组长进程和会话首进程
-$sid = posix_setsid();
-
-if ($sid === -1) {
-    echo '会话创建失败' . PHP_EOL;
-} else {
-    echo '会话创建成功，sid：' . $sid . PHP_EOL;
-}
-
-printId();
-
-while (1) {
-    ;
-}
+msg_receive($queue, 0, $msgType, 1024, $msg, true, MSG_NOERROR);
+echo "msgType：$msgType msg：$msg" . PHP_EOL;
