2b9b9df5c749c5e571a5e92541563cd9630af8d6
wangyaxian <wangyaxiandev@gmail.com>
2021-10-10 15:51:55 +0800

    IPC

--- test.php
+++ test.php
@@ -1,37 +1,35 @@
 <?php
 
 $key = ftok(__FILE__, 'a');
-$queue = msg_get_queue($key);
 
+$file = '/home/work/www/data.log';
+
+$count = 0;
+file_put_contents($file, $count);
+
+// 父子进程各自读取文件内容，然后内容 +1，再写回文件，这个过程循环 1k 次
 $pid = pcntl_fork();
 
 if ($pid === 0) {
-    while (1) {
-        // MSG_IPC_NOWAIT：非阻塞，立即返回；（如果使用非阻塞方式，该函数调用的次数会非常高，所以占用 CPU 资源就会高）
-        msg_receive($queue, 0, $msgType, 1024, $msg, true, MSG_IPC_NOWAIT, $error);
-        if ($error != MSG_ENOMSG) {
-            echo "msgType：$msgType msg：$msg" . PHP_EOL;
-        }
+    for ($i = 0; $i < 1000; $i++) {
+        $x = (int)file_get_contents($file);
+        $x += 1;
+        file_put_contents($file, $x);
     }
+
+    exit();
 }
 
-$i = 0;
-
-while (1) {
-    if ($i++ === 3) {
-        posix_kill($pid, SIGKILL);
-        break;
-    }
-    // 往消息队列发送一条消息
-    msg_send($queue, 2, 'hello', true);
-    sleep(1);
+for ($i = 0; $i < 1000; $i++) {
+    $x = (int)file_get_contents($file);
+    $x += 1;
+    file_put_contents($file, $x);
 }
 
-// 回收子进程
 $pid = pcntl_wait($status);
 if ($pid > 0) {
-    echo "子进程 $pid 退出了" . PHP_EOL;
+    echo "子进程 pid：$pid 退出了" . PHP_EOL;
 }
 
-// 删除消息队列
-msg_remove_queue($queue);
+$count = (int)file_get_contents($file);
+echo "count：$count" . PHP_EOL;
--- test2.php
+++ test2.php
@@ -1,7 +1,48 @@
 <?php
 
-$key = ftok('/home/work/www/test.php', 'a');
-$queue = msg_get_queue($key);
+$key = ftok(__FILE__, 'a');
+$semaphore = sem_get($key);
 
-msg_receive($queue, 0, $msgType, 1024, $msg, true, MSG_NOERROR);
-echo "msgType：$msgType msg：$msg" . PHP_EOL;
+$file = '/home/work/www/data.log';
+
+$count = 0;
+file_put_contents($file, $count);
+
+// 父子进程各自读取文件内容，然后内容 +1，再写回文件，这个过程循环 1k 次
+$pid = pcntl_fork();
+
+if ($pid === 0) {
+    for ($i = 0; $i < 1000; $i++) {
+        // 获得信号量
+        sem_acquire($semaphore);
+
+        $x = (int)file_get_contents($file);
+        $x += 1;
+        file_put_contents($file, $x);
+
+        // 释放信号量
+        sem_release($semaphore);
+    }
+
+    exit();
+}
+
+for ($i = 0; $i < 1000; $i++) {
+    // 获得信号量
+    sem_acquire($semaphore);
+
+    $x = (int)file_get_contents($file);
+    $x += 1;
+    file_put_contents($file, $x);
+
+    // 释放信号量
+    sem_release($semaphore);
+}
+
+$pid = pcntl_wait($status);
+if ($pid > 0) {
+    echo "子进程 pid：$pid 退出了" . PHP_EOL;
+}
+
+$count = (int)file_get_contents($file);
+echo "count：$count" . PHP_EOL;
