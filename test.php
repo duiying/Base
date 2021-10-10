879c4942baf02c94558d197b1618218a2cd9d622
wangyaxian <wangyaxiandev@gmail.com>
2021-10-10 16:30:11 +0800

    IPC

--- test.php
+++ test.php
@@ -1,35 +1,6 @@
 <?php
 
-$key = ftok(__FILE__, 'a');
-
-$file = '/home/work/www/data.log';
-
-$count = 0;
-file_put_contents($file, $count);
-
-// 父子进程各自读取文件内容，然后内容 +1，再写回文件，这个过程循环 1k 次
-$pid = pcntl_fork();
-
-if ($pid === 0) {
-    for ($i = 0; $i < 1000; $i++) {
-        $x = (int)file_get_contents($file);
-        $x += 1;
-        file_put_contents($file, $x);
-    }
-
-    exit();
-}
-
-for ($i = 0; $i < 1000; $i++) {
-    $x = (int)file_get_contents($file);
-    $x += 1;
-    file_put_contents($file, $x);
-}
-
-$pid = pcntl_wait($status);
-if ($pid > 0) {
-    echo "子进程 pid：$pid 退出了" . PHP_EOL;
-}
-
-$count = (int)file_get_contents($file);
-echo "count：$count" . PHP_EOL;
+$file = '/home/work/www/test.php';
+$key = ftok($file, 'a');
+$shm_id = shmop_open($key, 'c', 0666, 128);
+shmop_write($shm_id, 'hello', 0);
--- test2.php
+++ test2.php
@@ -1,48 +1,10 @@
 <?php
 
-$key = ftok(__FILE__, 'a');
-$semaphore = sem_get($key);
-
-$file = '/home/work/www/data.log';
-
-$count = 0;
-file_put_contents($file, $count);
-
-// 父子进程各自读取文件内容，然后内容 +1，再写回文件，这个过程循环 1k 次
-$pid = pcntl_fork();
-
-if ($pid === 0) {
-    for ($i = 0; $i < 1000; $i++) {
-        // 获得信号量
-        sem_acquire($semaphore);
-
-        $x = (int)file_get_contents($file);
-        $x += 1;
-        file_put_contents($file, $x);
-
-        // 释放信号量
-        sem_release($semaphore);
-    }
-
-    exit();
-}
-
-for ($i = 0; $i < 1000; $i++) {
-    // 获得信号量
-    sem_acquire($semaphore);
-
-    $x = (int)file_get_contents($file);
-    $x += 1;
-    file_put_contents($file, $x);
-
-    // 释放信号量
-    sem_release($semaphore);
-}
-
-$pid = pcntl_wait($status);
-if ($pid > 0) {
-    echo "子进程 pid：$pid 退出了" . PHP_EOL;
-}
-
-$count = (int)file_get_contents($file);
-echo "count：$count" . PHP_EOL;
+$file = '/home/work/www/test.php';
+$key = ftok($file, 'a');
+$shm_id = shmop_open($key, 'c', 0666, 128);
+echo shmop_read($shm_id, 0, 5) . PHP_EOL;
+// 关闭共享内存段，它的原理是把共享内存段与进程的地址空间映射关系给断开，并不会删除共享内存
+shmop_close($shm_id);
+// 如果想删除共享内存段，需要使用 shmop_delete
+shmop_delete($shm_id);
